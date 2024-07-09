d37f75a2ad1c12cf78cd4afdc3de6fe8c4074f88
jinh0318a <jinh0318a@naver.com>
2024-07-09 14:35:43 +0900

    login

---
+++ hello-002/js/login.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const login_body = document.querySelector("section.login.body");
+  const btn_login = login_body.querySelector("input.login");
+  const btn_join = login_body.querySelector("button.join");
+  const input_username = login_body.querySelector("input[name='username']");
+  const input_password = login_body.querySelector("input[name='password']");
+});
