166e1d728ac5663f6e8b43529e3fbcb4245e9b8e
jinh0318a <jinh0318a@naver.com>
2024-07-10 13:30:47 +0900

    hello004end

--- hello-003/js/detail.js
+++ hello-003/js/detail.js
@@ -3,12 +3,31 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const importHTML = (tag, url) => {
     const xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = () => {
+      tag.innerHTML = xhr.responseText;
+    };
+    xhr.send();
   };
   const onImageClickHandler = (e) => {
     const target = e.target;
     if (target.tagName !== "IMG") return false;
     const altText = e.target.getAttribute("alt");
-    alert(altText);
+    let url = "";
+    if (altText === "몽이인증") {
+      url = "./dog_detail_1.html";
+    } else if (altText === "스피츠종") {
+      url = "./dog_detail_2.html";
+    } else if (altText === "바라보기") {
+      url = "./dog_detail_3.html";
+    } else if (altText === "눈치보기") {
+      url = "./dog_detail_4.html";
+    } else if (altText === "놀아줘!") {
+      url = "./dog_detail_5.html";
+    } else if (altText === "첫만남") {
+      url = "./dog_detail_6.html";
+    }
+    importHTML(container, url);
   };
   container.addEventListener("click", onImageClickHandler);
 });
